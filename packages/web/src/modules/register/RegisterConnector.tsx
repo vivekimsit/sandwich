90d8ef8cd77deb7d19fbe1bcf1257a4f35c302fb
Vivek Poddar <vivekimsit@gmail.com>
2018-08-29 16:33:06 +0100

    Register form.

---
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { RegisterView } from "./ui/RegisterView";
+
+export class RegisterConnector extends React.PureComponent {
+  handleSubmit = async (values: any) => {
+    console.log(values);
+    return null;
+  };
+
+  render() {
+    return <RegisterView submit={this.handleSubmit} />;
+  }
+}
