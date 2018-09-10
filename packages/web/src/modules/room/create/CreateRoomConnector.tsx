131b6b337a71c1afbddfc660be99ef5e5ec5d4f2
Vivek Poddar <vivekimsit@gmail.com>
2018-09-10 16:13:24 +0100

    Room connector.

---
+++ packages/web/src/modules/room/create/CreateRoomConnector.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { FormikActions } from "formik";
+import { withCreateRoom, WithCreateRoom } from "@sandwich/controller";
+import { RoomFormValues, RoomForm } from "../shared/RoomForm";
+
+class C extends React.PureComponent<
+  RouteComponentProps<{ hotelId: string }> & WithCreateRoom
+> {
+  submit = async (
+    values: RoomFormValues,
+    { setSubmitting }: FormikActions<RoomFormValues>
+  ) => {
+    await this.props.createAddress({
+      input: values
+    });
+    setSubmitting(false);
+    this.props.history.push("/hotels");
+  };
+
+  render() {
+    const {
+      match: {
+        params: { hotelId }
+      }
+    } = this.props;
+    return <RoomForm hotelId={hotelId} submit={this.submit} />;
+  }
+}
+
+export const CreateRoomConnector = withCreateRoom(C);
