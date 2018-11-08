70864ae7d5f6f394df48c7b2e88289162527a275
Vivek Poddar <vivekimsit@gmail.com>
2018-11-08 10:50:45 +0000

    Add user avatar.

--- packages/web/src/modules/navbar/ui/NavBar.tsx
+++ packages/web/src/modules/navbar/ui/NavBar.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { MeQuery_me } from "@sandwich/controller";
 
 import { NavContainer, HomeTab, HotelsTab, ProfileTab, Label } from "./style";
+import { Navatar } from "./Navatar";
 
 interface IProps {
   currentUser: MeQuery_me;
@@ -19,16 +20,10 @@ export class NavBar extends React.PureComponent<IProps> {
         <HotelsTab to="/hotels">
           <Label>Hotels</Label>
         </HotelsTab>
-        {loggedIn && (
-          <ProfileTab to="/logout">
-            <Label>Logout</Label>
-          </ProfileTab>
-        )}
-        {!loggedIn && (
-          <ProfileTab to="/login">
-            <Label>Login</Label>
-          </ProfileTab>
-        )}
+        <ProfileTab to={loggedIn ? `/users/${currentUser.email}` : "/logout"}>
+          {loggedIn && <Navatar user={currentUser} />}
+          {!loggedIn && <Label>Login</Label>}
+        </ProfileTab>
       </NavContainer>
     );
   }
